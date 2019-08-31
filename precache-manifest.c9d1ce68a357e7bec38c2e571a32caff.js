4c122e14b7bc0e25b419edb9780b67ceb7a4184b
ana_smir <pl_nastya@mail.ru>
2019-08-31 16:05:15 +0300

    Updates

--- precache-manifest.c9d1ce68a357e7bec38c2e571a32caff.js
+++ precache-manifest.c9d1ce68a357e7bec38c2e571a32caff.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06dcfada42709b22e2ab979cc0650353",
+    "revision": "73a20fca353d0e3429c0caf5e55ea48f",
     "url": "/games/index.html"
   },
   {
-    "revision": "9d7c02cdd0525059c743",
+    "revision": "3d7b49a5c252f413d9c7",
     "url": "/games/static/css/main.ebf16c76.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/games/static/js/2.9a8a81db.chunk.js"
   },
   {
-    "revision": "9d7c02cdd0525059c743",
-    "url": "/games/static/js/main.9d1b31cd.chunk.js"
+    "revision": "3d7b49a5c252f413d9c7",
+    "url": "/games/static/js/main.23cc8a76.chunk.js"
   },
   {
     "revision": "bd248bff26a526f6018b",
