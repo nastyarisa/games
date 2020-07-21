796484946a99eabc1d7d0836044531e3fe56c6f1
Анастасия Смирнова <pl_nastya@mail.ru>
2020-07-21 23:41:20 +0300

    change background-color

---
+++ build/precache-manifest.1f4175846935718c6928b3146ba6264c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e0669dfcaee2a3f67b496b0179a86f",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "69ec96032ef07ec19146",
+    "url": "/games/static/css/main.946968dd.chunk.css"
+  },
+  {
+    "revision": "a302a482781a0fc950be",
+    "url": "/games/static/js/2.880fa4f8.chunk.js"
+  },
+  {
+    "revision": "69ec96032ef07ec19146",
+    "url": "/games/static/js/main.4f6c81c9.chunk.js"
+  },
+  {
+    "revision": "bd248bff26a526f6018b",
+    "url": "/games/static/js/runtime~main.ccf7e7e0.js"
+  },
+  {
+    "revision": "cc6762692cf70459cdebeaaf0bdcc52f",
+    "url": "/games/static/media/cross.cc676269.svg"
+  },
+  {
+    "revision": "9b8ae107e385f3a4c3e3e46bde53ee60",
+    "url": "/games/static/media/zero.9b8ae107.svg"
+  }
+]);
